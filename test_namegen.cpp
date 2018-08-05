4760b111e93a3002258c1426e1ec33fc5b4f77f3
Marco Pappalepore <crillion@tiscali.it>
2018-08-05 15:37:36 +0200

    namegen

--- test_namegen.cpp
+++ test_namegen.cpp
@@ -2,13 +2,51 @@
 #include <iostream>
 #include "namegen.hpp"
 
-int main()
+void test1()
 {
-	std::cout << "ciao!" << std::endl;
-	
 	auto s = merak::new_lexic(
 		"pitagora pisone callimaco filippo filottete ermete giapeto titano ariel umbriel ganimede andromeda marte giove carlo piero doriano giuliano protagora anassagora anassimene eraclito erone giasone gianluca callisto venere rea europa dione eurialo euripide sofocle ulisse agamennone achille eratostene eubea lesbo atene pandora oplonte uroboro ettore patroclo euclonte eschilo tucidide omero eneade ilio elio iperione", 
 		10);
 		
-	std::cout << s << std::endl;
+	std::cout << s << std::endl;	
+}
+
+void test2()
+{
+	std::cout << "specify how many new names generate : ";
+	int n = 0;
+	std::cin >> n;	
+	std::cout << "please insert some words or names just separed by space\n";
+
+
+	std::vector<std::string> items;
+	
+    std::copy(
+		std::istream_iterator<std::string>(std::cin),
+		std::istream_iterator<std::string>(),
+		std::back_inserter(items));
+
+/*		
+	std::stringstream oss;
+		
+	std::ostream_iterator<std::string> out(oss, " ");
+	
+	std::copy(
+		std::begin(items),
+		std::end(items),
+		out);
+	
+	std::string names = oss.str();
+	
+	auto new_names = merak::new_lexic(names, n);
+//	std::cout << new_names << std::endl;
+
+	std::cout << n << "\n";
+	std::cout << names << "\n" << std::endl;
+*/	
+}
+
+int main()
+{	
+	test2();
 }
